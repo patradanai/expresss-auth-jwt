19ad076a09a7b24eab3c7d428c6b45ba304c2b6f
patradanai <bloody_jojo@hotmail.com>
2021-04-03 00:23:12 +0700

    add Signin compare jwt and .env

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -4,7 +4,37 @@ const db = require("../models");
 const User = db.user;
 const Role = db.role;
 
-const signIn = (req, res) => {};
+const signIn = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check user,pass valid
+  if (!username || !password) {
+    return res.status(400).json({ message: "Invalid User or Password" });
+  }
+
+  // Find User in Db
+  const user = await User.findOne({ where: { username: username } });
+  if (!user) {
+    return res.status(400).json({ message: "User not existing" });
+  }
+
+  // Compare Password
+  const comparePassword = bcrypt.compareSync(password, user.password);
+  if (!comparePassword) {
+    return res.status(404).json({ message: "Password or User wrong" });
+  }
+
+  // Gen Token
+  const token = jwt.sign(
+    { id: user.id, name: user.username },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: 86400,
+    }
+  );
+
+  return res.status(200).json({ token: token });
+};
 
 const signUp = async (req, res) => {
   const { username, password, email, firstname, lastname } = req.body;
