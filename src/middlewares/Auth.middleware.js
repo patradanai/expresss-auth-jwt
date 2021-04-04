7c2169625af6fcba623231581213f2add5f6482f
patradanai <bloody_jojo@hotmail.com>
2021-04-04 16:35:57 +0700

    add middleware authorization

--- src/middlewares/Auth.middleware.js
+++ src/middlewares/Auth.middleware.js
@@ -1,5 +1,27 @@
+const jwt = require("jsonwebtoken");
 const db = require("../models");
 const User = db.user;
+
+const isAuthorize = (req, res) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(400).json({ message: "No Token Proiving" });
+  }
+
+  // Split Header Bearer xxx
+  const splitToken = authorization.split(" ");
+
+  // Verify Token
+  jwt.verify(splitToken[1], process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(400).json({ message: "Unathorized!!!!" });
+    }
+
+    req.user = user.id;
+    next();
+  });
+};
 
 const IsExistUser = async (req, res, next) => {
   const { username } = req.body;
@@ -21,4 +43,5 @@ const IsExistUser = async (req, res, next) => {
 
 module.exports = {
   IsExistUser: IsExistUser,
+  isAuthorize: isAuthorize,
 };
